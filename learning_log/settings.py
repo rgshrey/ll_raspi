89b73963ba501f7341771bc0b918ee00e851e3fe
rgshrey <rgshrey@gmail.com>
2017-07-08 02:42:59 -0400

    Update settings.py

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # My settings
 LOGIN_URL = '/users/login/'
@@ -150,7 +151,7 @@ if os.getcwd() == '/app':
     #Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+    STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
